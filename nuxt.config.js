be3f193f7078890696e327e0cb2e70f933fa2a19
logotip4ik <bogdankostyuk12@gmail.com>
2021-06-29 14:39:30 +0300

    update: added basic typer

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,31 +1,30 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-
+  loading: {
+    color: 'cyan',
+    height: '3px',
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'typist-nuxt',
+    title: 'Typist',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: ['~/assets/css/main.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -34,6 +33,7 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
+    'nuxt-vite',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -46,6 +46,6 @@ export default {
   axios: {},
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {},
+  vite: { ssr: true },
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,139 @@
 <template>
-  <Tutorial/>
+  <div class="main">
+    <h1 class="main__header">🎉 Typist 🎉</h1>
+    <div class="main__text">
+      <p>
+        <span
+          v-for="(letter, idx) in rawText"
+          :key="idx"
+          :class="{
+            'curr-letter': idx === currLetter,
+            'passed-letter': idx < currLetter,
+            'wrong-letter': userText[idx] !== letter && idx < currLetter,
+          }"
+          >{{ letter }}</span
+        >
+      </p>
+      <input ref="input" class="main__input" @input="addValue" />
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  async asyncData({ $axios, query }) {
+    const RANDOM_URL =
+      'https://api.quotable.io/random?minLength=50?maxLength=150'
+    const ID_URL = 'https://api.quotable.io/quotes/'
+
+    const url = query.q ? `${ID_URL}${decodeURI(query.q)}` : RANDOM_URL
+    const { data } = await $axios.get(url)
+
+    const rawText = data.content.split('')
+
+    return { rawText, idText: data._id }
+  },
+  data: () => ({
+    userText: [],
+    currLetter: 0,
+    numWords: 0,
+  }),
+  computed: {},
+  mounted() {
+    this.setQueryParam()
+    this.$refs.input.focus()
+  },
+  methods: {
+    setQueryParam() {
+      const params = new URLSearchParams(window.location.search)
+      params.set('q', encodeURI(this.idText))
+      window.history.replaceState({}, '', `?${params.toString()}`)
+    },
+    isWordsCorrect() {
+      let isCorrect = true
+
+      const range = this.rawText.slice(0, this.currLetter).length
+      for (let i = 0; i < range; i += 1) {
+        if (this.userText[i] !== this.rawText[i]) {
+          isCorrect = false
+          break
+        }
+      }
+      return isCorrect
+    },
+    addValue({ target, inputType, data }) {
+      if (inputType === 'insertText') {
+        this.userText.push(target.value[target.value.length - 1])
+        this.currLetter += 1
+        const isCorrect = this.isWordsCorrect()
+        if (data === ' ' && isCorrect) {
+          this.$refs.input.value = ''
+          this.numWords += 1
+        }
+        if (this.currLetter > this.rawText.length && isCorrect) {
+          console.log('finished')
+        }
+      } else if (inputType === 'deleteContentBackward') {
+        this.currLetter -= 1
+        this.userText.pop()
+      }
+    },
+  },
+}
 </script>
+
+<style lang="scss">
+.main {
+  text-align: center;
+  max-width: 700px;
+  margin: 0 auto;
+
+  &__header {
+    padding-top: 5rem;
+    font-size: 60px;
+    text-align: center;
+    margin-bottom: 5rem;
+  }
+
+  &__text p {
+    text-align: center;
+    font-size: 30px;
+    word-wrap: break-word;
+
+    span {
+      position: relative;
+      transition: background 100ms linear;
+
+      &.passed-letter {
+        background: rgba(lightgreen, 0.5);
+      }
+      &.curr-letter::after {
+        content: ' ';
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        height: 2px;
+        background: rgba(cyan, 1);
+        animation: loading 1s infinite ease-out;
+      }
+      &.wrong-letter {
+        background: rgba($color: red, $alpha: 0.5);
+      }
+    }
+  }
+  &__input {
+    color: white;
+    padding: 1rem;
+    margin-top: 5rem;
+    font-size: 30px;
+    width: 100%;
+    appearance: none;
+    background: transparent;
+    border: none;
+    box-shadow: 0 0 5px cyan;
+    border-radius: 0.25rem;
+    outline: none;
+  }
+}
+</style>
