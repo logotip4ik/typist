fc7ec2ecbdc7b14013bd27c59692a17433b74993
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-04-08 22:45:02 +0300

    update: added some basic data fetching

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <pre>
+      {{ data }}
+    </pre>
+  </div>
+</template>
+
+<script setup>
+const { data } = await useAsyncData('random-text', () =>
+  $fetch('/api/get-random-text')
+);
+</script>
